b33a44ea136958f0bd0c9c4db4737357381884e5
Mauro Sergio Ferreira Brasil <msfbrasil@gmail.com>
2022-07-17 11:43:02 -0300

    - Implemented send notification from SalesCard's button with integration between 'SalesCard' and 'NotificationButton' components.

--- frontend/src/components/notification-button/NotificationButton.tsx
+++ frontend/src/components/notification-button/NotificationButton.tsx
@@ -1,10 +1,25 @@
+import axios from 'axios';
+
 import notitication_icon from '../../assets/images/notification-icon.svg';
+
+import { BASE_URL } from '../../utils/request';
 
 import './styles.css'
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+}
+
+function handleClick( id: number ) {
+  axios(`${BASE_URL}/sales/${id}/notification`)
+    .then( response => {
+      console.log("Sucesso!");
+    });
+}
+
+function NotificationButton( {saleId} : Props ) {
   return ( 
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={ () => handleClick( saleId )}>
       <img src={notitication_icon} alt="Notificar"/>
     </div>
   )
